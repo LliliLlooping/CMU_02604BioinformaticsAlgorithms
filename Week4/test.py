02692cb937d908be0b6986b05bd1339bb1cefe0a
Li Chen <lichen.codes@gmail.com>
2024-02-08 10:40:16 -0500

    Finishes the Longest Common Subsequence problem in Chapter 4

--- Week4/test.py
+++ Week4/test.py
@@ -1,8 +1,12 @@
 from MoneyChanging import  MoneyChanging
 from ManhattanTourist import ManhattanTourist
+from LCSeq import LongestCommonSubsequence
 
-# money_changing = MoneyChanging()
-# money_changing.test()
+money_changing = MoneyChanging()
+money_changing.test()
 
-# manhattan_tourist = ManhattanTourist()
-# manhattan_tourist.test()
+manhattan_tourist = ManhattanTourist()
+manhattan_tourist.test()
+
+lcseq = LongestCommonSubsequence()
+lcseq.test()
