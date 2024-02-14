eeb0c8b6ef39fedb37acdcbea53aba6b115c7c4f
Li Chen <lichen.codes@gmail.com>
2024-02-14 15:08:09 -0500

    Finishes Local Alignment in Chapter 5

--- Week5/Alignment.py
+++ Week5/Alignment.py
@@ -8,11 +8,21 @@ class Alignment:
     def __init__(self):
         pass
     
+    def local_alignment(self, match: int, mismatch: int, indel: int, s1: str, s2: str):
+        
+        score, backtrack, [i, j] = self.local_alignment_backtrack(match, mismatch, indel, s1, s2)
+        gapped_s1, gapped_s2 = [], []
+        self.output(backtrack, gapped_s1, gapped_s2, s1, s2, i, j)
+        
+        gapped_s1, gapped_s2 = ''.join(gapped_s1[::-1]), ''.join(gapped_s2[::-1])
+        
+        return (score, gapped_s1, gapped_s2)
+    
     def global_alignment(self, match: int, mismatch: int, indel: int, s1: str, s2: str):
         
         score, backtrack = self.global_alignment_backtrack(match, mismatch, indel, s1, s2)
         gapped_s1, gapped_s2 = [], []
-        self.global_output(backtrack, gapped_s1, gapped_s2, s1, s2, len(s1), len(s2))
+        self.output(backtrack, gapped_s1, gapped_s2, s1, s2, len(s1), len(s2))
         
         gapped_s1, gapped_s2 = ''.join(gapped_s1[::-1]), ''.join(gapped_s2[::-1])
         
@@ -56,11 +66,12 @@ class Alignment:
                     backtrack[i, j] = 2
         return score[len(s1), len(s2)], backtrack
     
-    def global_output(self, backtrack, gapped_s1, gapped_s2, s1, s2, i, j):
+    
+    def output(self, backtrack, gapped_s1, gapped_s2, s1, s2, i, j):
         """
             Recursively output based on an backtrack array.
         """
-        if i == 0 and j == 0:
+        if backtrack[i, j] == 0:
             return
         if backtrack[i, j] == 1: # horizontal
             gapped_s1.append('-')
@@ -78,9 +89,45 @@ class Alignment:
             self.output(backtrack, gapped_s1, gapped_s2, s1, s2, i - 1, j - 1)
             return
         
+    def local_alignment_backtrack(self, match: int, mismatch: int, indel: int, s1: str, s2: str) -> Tuple[int, np.array]:
+        
+        l1, l2 = len(s1), len(s2)
+        
+        score, backtrack = np.zeros((len(s1) + 1, len(s2) + 1), dtype=int), np.zeros((len(s1) + 1, len(s2) + 1), dtype=int)
+            
+        for i in range(1, l1+1):
+            for j in range(1, l2+1):
+                
+                if s1[i-1] == s2[j-1]:
+                    diag = score[i-1, j-1] + match
+                else:
+                    diag = score[i-1, j-1] - mismatch
+                up = score[i-1, j] - indel
+                left = score[i, j-1] - indel
+                
+                if 0 == max(diag, up, left, 0):
+                    score[i, j] = 0
+                    
+                elif diag == max(diag, up, left, 0):
+                    score[i, j] = diag
+                    backtrack[i, j] = 3
+                    
+                elif left == max(diag, up, left, 0):
+                    score[i, j] = left
+                    backtrack[i, j] = 1
+                    
+                else:
+                    score[i, j] = up
+                    backtrack[i, j] = 2
+                    
+        print(score)
+        print(backtrack)
+        i, j = np.argmax(score) // score.shape[1], np.argmax(score) % score.shape[1]
+        return score[i, j], backtrack, [i, j]
+    
     def test(self):
-        match, mismatch, indel = 2, 5, 1
-        s1 = 'TCA'
-        s2 = 'CA'
-        score, gapped_s1, gapped_s2 = self.global_alignment(match, mismatch, indel, s1, s2)
+        match, mismatch, indel = 1, 1, 1
+        s1 = 'TAACG'
+        s2 = 'ACGTG'
+        score, gapped_s1, gapped_s2 = self.local_alignment(match, mismatch, indel, s1, s2)
         print(score, gapped_s1, gapped_s2)
